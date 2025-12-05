6e8876b5cb1bb0e8d44a2be1f13fd41ab631e1b9
sunwenqiang-js <--list>
2025-12-05 15:32:06 +0800

    feat:LoginUser实体增加partnerId字段

--- bajiecloud-spring-boot-starter-security/src/main/java/com/bajiezu/cloud/framework/security/LoginUser.java
+++ bajiecloud-spring-boot-starter-security/src/main/java/com/bajiezu/cloud/framework/security/LoginUser.java
@@ -17,54 +17,59 @@ import java.util.Map;
 @Accessors(chain = true)
 public class LoginUser<T extends LoginInfoEntity> implements Serializable {
 
-  /**
-   * 用户编号
-   */
-  private Long id;
-  /**
-   * 用户类型
-   * <p>
-   * 关联 {@link com.bajie.cloud.common.constants.UserTypeEnum}
-   */
-  private Integer userType;
+    /**
+     * 用户编号
+     */
+    private Long id;
+    /**
+     * 用户类型
+     * <p>
+     * 关联 {@link com.bajie.cloud.common.constants.UserTypeEnum}
+     */
+    private Integer userType;
 
-  /**
-   * 登陆用户额外信息
-   */
-  private T loginInfo;
+    /**
+     * 登陆用户额外信息
+     */
+    private T loginInfo;
 
-  /**
-   * 过期时间
-   */
-  private LocalDateTime expiresTime;
+    /**
+     * 过期时间
+     */
+    private LocalDateTime expiresTime;
 
-  private String ip;
+    private String ip;
 
-  private String token;
+    private String token;
 
-  // ========== 上下文 ==========
-  /**
-   * 上下文字段，不进行持久化
-   * <p>
-   * 1. 用于基于 LoginUser 维度的临时缓存
-   */
-  @JsonIgnore
-  private Map<String, Object> context;
+    /**
+     * 合作商id
+     */
+    private Long partnerId;
+
+    // ========== 上下文 ==========
+    /**
+     * 上下文字段，不进行持久化
+     * <p>
+     * 1. 用于基于 LoginUser 维度的临时缓存
+     */
+    @JsonIgnore
+    private Map<String, Object> context;
 
 
-  public String getUsername() {
-    return loginInfo != null ? loginInfo.getUsername() : null;
-  }
-
-  public void setContext(String key, Object value) {
-    if (context == null) {
-      context = new HashMap<>();
+    public String getUsername() {
+        return loginInfo != null ? loginInfo.getUsername() : null;
     }
-    context.put(key, value);
-  }
 
-  public <R> R getContext(String key, Class<R> type) {
-    return MapUtil.get(context, key, type);
-  }
+    public void setContext(String key, Object value) {
+        if (context == null) {
+            context = new HashMap<>();
+        }
+        context.put(key, value);
+    }
+
+    public <R> R getContext(String key, Class<R> type) {
+        return MapUtil.get(context, key, type);
+    }
 
 }
