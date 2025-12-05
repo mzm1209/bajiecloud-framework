079b3cf2a939ffb573f41d0795d326735a4d326d
sunwenqiang-js <--list>
2025-12-05 15:34:39 +0800

    feat:调整CommonStatusEnum的值

--- bajiecloud-common/src/main/java/com/bajie/cloud/common/constants/CommonStatusEnum.java
+++ bajiecloud-common/src/main/java/com/bajie/cloud/common/constants/CommonStatusEnum.java
@@ -2,9 +2,10 @@ package com.bajie.cloud.common.constants;
 
 import cn.hutool.core.util.ObjUtil;
 import com.bajie.cloud.common.type.ArrayValuable;
-import java.util.Arrays;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.Arrays;
 
 /**
  * 通用状态枚举
@@ -13,32 +14,32 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum CommonStatusEnum implements ArrayValuable<Integer> {
 
-  ENABLE(0, "开启"),
-  DISABLE(1, "关闭");
+    ENABLE(1, "开启"),
+    DISABLE(0, "关闭");
 
-  public static final Integer[] ARRAYS = Arrays.stream(values()).map(CommonStatusEnum::getStatus)
-      .toArray(Integer[]::new);
+    public static final Integer[] ARRAYS = Arrays.stream(values()).map(CommonStatusEnum::getStatus)
+            .toArray(Integer[]::new);
 
-  /**
-   * 状态值
-   */
-  private final Integer status;
-  /**
-   * 状态名
-   */
-  private final String name;
+    /**
+     * 状态值
+     */
+    private final Integer status;
+    /**
+     * 状态名
+     */
+    private final String name;
 
-  public static boolean isEnable(Integer status) {
-    return ObjUtil.equal(ENABLE.status, status);
-  }
+    public static boolean isEnable(Integer status) {
+        return ObjUtil.equal(ENABLE.status, status);
+    }
 
-  public static boolean isDisable(Integer status) {
-    return ObjUtil.equal(DISABLE.status, status);
-  }
+    public static boolean isDisable(Integer status) {
+        return ObjUtil.equal(DISABLE.status, status);
+    }
 
-  @Override
-  public Integer[] array() {
-    return ARRAYS;
-  }
+    @Override
+    public Integer[] array() {
+        return ARRAYS;
+    }
 
 }
