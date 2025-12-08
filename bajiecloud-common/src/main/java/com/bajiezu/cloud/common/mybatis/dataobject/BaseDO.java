38eedaa7cedb35d9569207af169c21899fa90b6e
sunwenqiang-js <--list>
2025-12-08 15:22:07 +0800

    feat:调整deleteFlag为isDeleted

--- bajiecloud-common/src/main/java/com/bajiezu/cloud/common/mybatis/dataobject/BaseDO.java
+++ bajiecloud-common/src/main/java/com/bajiezu/cloud/common/mybatis/dataobject/BaseDO.java
@@ -1,39 +1,40 @@
 package com.bajiezu.cloud.common.mybatis.dataobject;
 
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.Date;
-import lombok.Data;
 
 @Data
 public abstract class BaseDO implements Serializable {
 
-  /**
-   * 合作商id
-   */
-  private Long partnerId;
+    /**
+     * 合作商id
+     */
+    private Long partnerId;
 
-  /**
-   * 创建时间
-   */
-  private Date createTime;
+    /**
+     * 创建时间
+     */
+    private Date createTime;
 
-  /**
-   * 最后更新时间
-   */
-  private Date updateTime;
+    /**
+     * 最后更新时间
+     */
+    private Date updateTime;
 
-  /**
-   * 创建者，sys_user表的主键id
-   */
-  private Long createBy;
+    /**
+     * 创建者，sys_user表的主键id
+     */
+    private Long createBy;
 
-  /**
-   * 更新者，sys_user表的主键id
-   */
-  private Long updateBy;
+    /**
+     * 更新者，sys_user表的主键id
+     */
+    private Long updateBy;
 
-  /**
-   * 删除标识，0-正常，>0 删除时间戳（秒）
-   */
-  private Long deleteFlag;
+    /**
+     * 删除标识，0-正常，1-已删除
+     */
+    private Integer isDeleted;
 }
