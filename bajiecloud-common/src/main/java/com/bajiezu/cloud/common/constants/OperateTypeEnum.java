e36b65803bbe6f7abc0b99a65cb89d3fef352e3b
sunwenqiang-js <--list>
2025-12-12 11:36:59 +0800

    feat:新增通用操作类型枚举类

---
+++ bajiecloud-common/src/main/java/com/bajiezu/cloud/common/constants/OperateTypeEnum.java
@@ -0,0 +1,35 @@
+package com.bajiezu.cloud.common.constants;
+
+import com.bajiezu.cloud.common.type.ArrayValuable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+/**
+ * 全局操作类型枚举
+ */
+@AllArgsConstructor
+@Getter
+public enum OperateTypeEnum implements ArrayValuable<Integer> {
+
+    SAVE(1, "保存"),
+    SUBMIT(2, "提交");
+
+
+    public static final Integer[] ARRAYS = Arrays.stream(values()).map(OperateTypeEnum::getStatus).toArray(Integer[]::new);
+
+    /**
+     * 状态值
+     */
+    private final Integer status;
+    /**
+     * 状态名
+     */
+    private final String name;
+
+    @Override
+    public Integer[] array() {
+        return ARRAYS;
+    }
+}
